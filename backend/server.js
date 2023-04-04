8cd010f0e18a4316f9ab561d413395c518578f71
Irfan Thomson <irfanthomson@gmail.com>
2023-04-04 01:11:11 -0500

    process, self reflection, iterations, and printing

--- backend/server.js
+++ backend/server.js
@@ -18,38 +18,38 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Text analysis API');
+  res.send('Text process API');
 });
 
-app.post('/api/analyze-text', async (req, res) => {
+app.post('/api/process-text', async (req, res) => {
   try {
-    const { text, option, iterations = 0, guidance } = req.body;
+    const { text, option, iterations = 0, guidance, selfReflectionOption} = req.body;
 
     if (!text || !option) {
-      res.status(400).json({ success: false, message: 'Missing text or analysis option.' });
+      res.status(400).json({ success: false, message: 'Missing text or process option.' });
       return;
     }
 
     const maxIterations = 5;
     const effectiveIterations = Math.min(iterations, maxIterations);
 
-    let analysisResult = await analyzeTextWithChatGPT(text, option, guidance);
+    let processResult = await processTextWithChatGPT(text, option, guidance);
 
-    analysisResult = await selfReflectOnAnalysis(analysisResult.result, effectiveIterations, guidance);
+    processResult = await selfReflectOnAnalysis(processResult.result, effectiveIterations, guidance, selfReflectionOption, text, option);
 
-    res.status(200).json(analysisResult);
+    res.status(200).json(processResult);
   } catch (error) {
-    console.error('Error in /api/analyze-text:', error.message);
+    console.error('Error in /api/process-text:', error.message);
     res.status(500).json({ success: false, message: 'An error occurred while processing the request.' });
   }
 });
 
 app.post('/api/generate-prompt', async (req, res) => {
   try {
-    const { text, option, iterations, guidance } = req.body;
+    const { text, option, iterations, guidance, selfReflectionOption } = req.body;
 
     if (!text || !option) {
-      res.status(400).json({ success: false, message: 'Missing text or analysis option.' });
+      res.status(400).json({ success: false, message: 'Missing text or process option.' });
       return;
     }
 
@@ -58,13 +58,29 @@ app.post('/api/generate-prompt', async (req, res) => {
 
     if (iterations && iterations > 0) {
       for (let i = 0; i < iterations; i++) {
-        const reflectionPrompt = 'Please perform a comprehensive and critical evaluation of the following text and identify its strengths and weaknesses in detail. Provide specific improvements and actionable suggestions, supported by examples. ${guidance}';
-        prompts.push(reflectionPrompt);
-        const improvementPrompt = 'Based on that critical analysis, please improve the original text. ${guidance}';
-        prompts.push(improvementPrompt);
+        let reflectionPrompt;
+        switch (selfReflectionOption) {
+          case 'default':
+            reflectionPrompt = `Refine the analysis of the original text:\n\n"${text}"\n\nOption: ${option}\n\nImprove the previous response:\n\n"${generatedPrompt}"\n\nConsider any flaws or inaccuracies without stating them explicitly and directly provide a higher-quality answer. ${guidance}`;
+            prompts.push(reflectionPrompt);
+            break;
+          case 'critical analysis':
+            reflectionPrompt = `Please perform a comprehensive and critical evaluation of the following text and identify its strengths and weaknesses in detail. Provide specific improvements and actionable suggestions, supported by examples. ${guidance}`;
+            prompts.push(reflectionPrompt);
+            const improvementPrompt = `Based on that critical analysis, please improve the original text. ${guidance}`;
+            prompts.push(improvementPrompt);
+            break;
+          case 'idea analysis':
+            reflectionPrompt = `Please perform a comprehensive and critical evaluation of the ideas presented in the following text. Identify the strengths and weaknesses of these ideas in detail. Provide specific improvements and actionable suggestions, supported by examples, to enhance the quality and depth of the ideas. ${guidance}`;
+            prompts.push(reflectionPrompt);
+            const ideaImprovementPrompt = `Based on the idea analysis provided, please improve the original text. ${guidance}`;
+            prompts.push(ideaImprovementPrompt);
+            break;
+          default:
+            throw new Error('Invalid self-reflection option.');
+        }
       }
     }
-
     res.status(200).json({ success: true, result: prompts.join('\n\n---\n\n') });
   } catch (error) {
     console.error('Error in /api/generate-prompt:', error.message);
@@ -72,7 +88,7 @@ app.post('/api/generate-prompt', async (req, res) => {
   }
 });
 
-async function analyzeTextWithChatGPT(text, option, guidance) {
+async function processTextWithChatGPT(text, option, guidance) {
   const prompt = generatePrompt(text, option, guidance);
 
   const response = await openai.createChatCompletion({
@@ -80,30 +96,41 @@ async function analyzeTextWithChatGPT(text, option, guidance) {
     messages: [{ role: 'user', content: prompt }],
   });
 
-  const analysisResult = processChatGptResponse(response.data.choices[0].message.content);
-  return { success: true, result: analysisResult };
+  const processResult = processChatGptResponse(response.data.choices[0].message.content);
+  return { success: true, result: processResult };
 }
 
-async function selfReflectOnAnalysis(initialResponse, numIterations, guidance) {
+async function selfReflectOnAnalysis(initialResponse, numIterations, guidance, selfReflectionOption, text, option) {
   let currentResponse = initialResponse;
 
   for (let i = 0; i < numIterations; i++) {
-    const analysisPrompt = generatePrompt(currentResponse, 'critical analysis');
-    
-    const analysisResponse = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: analysisPrompt }],
-    });
-    const analyzedResponse = processChatGptResponse(analysisResponse.data.choices[0].message.content);
+    if (selfReflectionOption === 'default') {
+      const defaultPrompt = `Refine the analysis of the original text:\n\n"${text}"\n\nOption: ${option}\n\nImprove the previous response:\n\n"${initialResponse}"\n\nConsider any flaws or inaccuracies without stating them explicitly and directly provide a higher-quality answer. ${guidance}`;
 
-    const improvementPrompt = `Based on the critical analysis provided, please improve the original text. ${guidance}\n\nOriginal Text: ${currentResponse}\n\nCritical Analysis: ${analyzedResponse}`;
-    
-    const improvedResponse = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: improvementPrompt }],
-    });
+      const defaultResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: defaultPrompt }],
+      });
 
-    currentResponse = processChatGptResponse(improvedResponse.data.choices[0].message.content);
+      currentResponse = processChatGptResponse(defaultResponse.data.choices[0].message.content);
+    } else {
+      const analysisPrompt = generatePrompt(currentResponse, selfReflectionOption, guidance);
+
+      const analysisResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: analysisPrompt }],
+      });
+      const analyzedResponse = processChatGptResponse(analysisResponse.data.choices[0].message.content);
+
+      const improvementPrompt = `Based on the analysis provided, please improve the original text. ${guidance}\n\nOriginal Text: ${currentResponse}\n\nAnalysis: ${analyzedResponse}`;
+
+      const improvedResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: improvementPrompt }],
+      });
+
+      currentResponse = processChatGptResponse(improvedResponse.data.choices[0].message.content);
+    }
   }
 
   return { success: true, result: currentResponse };
@@ -117,13 +144,16 @@ function generatePrompt(text, option, guidance) {
       prompt = `Using the following text as a starting point, create a detailed and engaging article that weaves these ideas together, explores their connections, and dives deeper into the subject matter. ${guidance}\n\n${text}`;
       break;
     case 'generate story':
-      prompt = 'Given the text below, construct a comprehensive outline for an enthralling story. Ensure that the outline covers key plot points, character development, subplots, and relevant themes. Use the provided ideas as a foundation to build a rich and engaging narrative. ${guidance}\n\n${text}';
+      prompt = `Given the text below, construct a comprehensive outline for an enthralling story. Ensure that the outline covers key plot points, character development, subplots, and relevant themes. Use the provided ideas as a foundation to build a rich and engaging narrative. ${guidance}\n\n${text}`;
       break;
     case 'critical analysis':
       prompt = `Please perform a comprehensive and critical evaluation of the following text and identify its strengths and weaknesses in detail. Provide specific improvements and actionable suggestions, supported by examples. ${guidance}\n\n${text}`;
       break;
+    case 'idea analysis':
+      prompt = `Please perform a comprehensive and critical evaluation of the ideas presented in the following text. Identify the strengths and weaknesses of these ideas in detail. Provide specific improvements and actionable suggestions, supported by examples, to enhance the quality and depth of the ideas. ${guidance}\n\n${text}`;
+      break;
     default:
-      throw new Error('Invalid analysis option.');
+      throw new Error('Invalid process option.');
   }
   return prompt;
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,20 @@ import './styles.css';
 
 function App() {
   const [textInput, setTextInput] = useState('');
-  const [analysisOption, setAnalysisOption] = useState('');
+  const [processOption, setProcessOption] = useState('');
   const [iterations, setIterations] = useState(1);
   const [guidance, setGuidance] = useState('');
+  const [selfReflectionOption, setSelfReflectionOption] = useState('default');
   const [outputText, setOutputText] = useState('');
 
-  async function analyzeText(text, option, iterations, guidance) {
+  async function processText(text, option, iterations, guidance, selfReflectionOption) {
     try {
-      const response = await fetch('http://localhost:3001/api/analyze-text', {
+      const response = await fetch('http://localhost:3001/api/process-text', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ text, option, iterations, guidance }),
+        body: JSON.stringify({ text, option, iterations, guidance, selfReflectionOption }),
       });
 
       if (!response.ok) {
@@ -25,7 +26,7 @@ function App() {
       const result = await response.json();
       return result;
     } catch (error) {
-      console.error('Error in analyzeText:', error.message);
+      console.error('Error in processText:', error.message);
       throw new Error('An error occurred while processing the request.');
     }
   }
@@ -34,8 +35,8 @@ function App() {
     setTextInput(event.target.value);
   }
 
-  function handleAnalysisOptionChange(event) {
-    setAnalysisOption(event.target.value);
+  function handleProcessOptionChange(event) {
+    setProcessOption(event.target.value);
   }
   function handleIterationsChange(event) {
     setIterations(event.target.value);
@@ -44,28 +45,31 @@ function App() {
   function handleGuidanceChange(event) {
     setGuidance(event.target.value);
   }
+  function handleSelfReflectionOptionChange(event) {
+    setSelfReflectionOption(event.target.value);
+  }
 
-  async function handleAnalyzeButtonClick() {
-    if (!textInput || !analysisOption) {
-      alert('Please enter text and select an analysis option.');
+  async function handleProcessButtonClick() {
+    if (!textInput || !processOption) {
+      alert('Please enter text and select a process option.');
       return;
     }
-
+  
     try {
-      const result = await analyzeText(textInput, analysisOption, iterations, guidance);
+      const result = await processText(textInput, processOption, iterations, guidance, selfReflectionOption);
       setOutputText(result.result);
     } catch (error) {
-      console.error('Error in handleAnalyzeButtonClick:', error.message);
+      console.error('Error in handleProcessButtonClick:', error.message);
     }
   }
-  async function printPrompts(text, option, iterations, guidance) {
+  async function printPrompts(text, option, iterations, guidance, selfReflectionOption) {
     try {
       const response = await fetch('http://localhost:3001/api/generate-prompt', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ text, option, iterations, guidance }),
+        body: JSON.stringify({ text, option, iterations, guidance, selfReflectionOption }),
       });
 
       if (!response.ok) {
@@ -81,13 +85,13 @@ function App() {
   }
 
   async function handlePrintPromptsButtonClick() {
-    if (!textInput || !analysisOption) {
-      alert('Please enter text and select an analysis option.');
+    if (!textInput || !processOption) {
+      alert('Please enter text and select an process option.');
       return;
     }
 
     try {
-      const result = await printPrompts(textInput, analysisOption, iterations, guidance);
+      const result = await printPrompts(textInput, processOption, iterations, guidance, selfReflectionOption);
       setOutputText(result.result);
     } catch (error) {
       console.error('Error in handlePrintPromptsButtonClick:', error.message);
@@ -95,7 +99,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Text Analysis with ChatGPT</h1>
+      <h1>Text Processing with ChatGPT</h1>
       <div className="TextInput">
         <textarea
           rows="20"
@@ -105,14 +109,23 @@ function App() {
           placeholder="Enter your text here"
         />
       </div>
-      <div className="AnalysisOptions">
+      <div className="ProcessOptions">
         <label>
-          Choose analysis type:
-          <select value={analysisOption} onChange={handleAnalysisOptionChange}>
+          Choose process:
+          <select value={processOption} onChange={handleProcessOptionChange}>
             <option value="">--Select an option--</option>
             <option value="generate article">Generate Article</option>
             <option value="generate story">Generation Story</option>
             <option value="critical analysis">Critical Analysis</option>
+            <option value="idea analysis">Idea Analysis</option>
+          </select>
+        </label>
+        <label>
+          Self-reflection option:
+          <select value={selfReflectionOption} onChange={handleSelfReflectionOptionChange}>
+            <option value="default">Default</option>
+            <option value="critical analysis">Critical Analysis</option>
+            <option value="idea analysis">Idea Analysis</option>
           </select>
         </label>
         <label>
@@ -136,7 +149,7 @@ function App() {
         </label>
       </div>
       <div className="Buttons">
-        <button onClick={handleAnalyzeButtonClick}>Analyze</button>
+        <button onClick={handleProcessButtonClick}>Process</button>
         <button onClick={handlePrintPromptsButtonClick}>Print Prompts</button>
       </div>
       <div className="Output">
